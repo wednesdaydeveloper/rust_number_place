30a75ae4abd732a314e04c654f392ee602c79994
wednesday.developer <wednesday.developer@gmail.com>
2024-12-25 21:05:45 +0900

    itertools を導入

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod sudoku {
+    use itertools::{iproduct, Itertools};
 
     #[derive(Debug, Clone, Copy)]
     pub struct Cell {
@@ -22,7 +23,7 @@ pub mod sudoku {
                 .map(|&solved_cell| solved_cell.val)
                 .collect::<Vec<u8>>();
 
-            return (1..=9).filter(|&n| !solved_numbers.contains(&n)).collect();
+            (1..=9).filter(|&n| !solved_numbers.contains(&n)).collect()
         }
     }
 
@@ -32,54 +33,54 @@ pub mod sudoku {
         pub val: u8,
     }
 
-    pub fn print(solved: &Vec<SolvedCell>) {
-        for row in 0..9 {
-            for col in 0..9 {
-                let n = match solved
-                    .iter()
-                    .find(|&solved_cell| solved_cell.cell.row == row && solved_cell.cell.col == col)
-                {
-                    Some(solved_cell) => solved_cell.val,
-                    None => 0,
-                };
-                print!("{:2} ", n);
-            }
-            println!();
-        }
+    fn to_row_col() -> itertools::Product<std::ops::Range<u8>, std::ops::Range<u8>> {
+        iproduct!(0..9, 0..9)
+    }
+
+    pub fn to_string(solved: &Vec<SolvedCell>) -> String {
+        to_row_col()
+            .map(|(row, col)| {
+                format!(
+                    "{:2}",
+                    match solved
+                        .iter()
+                        .find(|&solved_cell| solved_cell.cell.row == row
+                            && solved_cell.cell.col == col)
+                    {
+                        Some(solved_cell) => solved_cell.val,
+                        None => 0,
+                    }
+                )
+            })
+            .chunks(9) //  1行ごとにまとめる
+            .into_iter()
+            .map(|mut x| x.join(" "))
+            .join("\r\n")
     }
 
     pub fn solve_from_array(array: &[[u8; 9]; 9]) -> Option<Vec<SolvedCell>> {
-        let mut solved: Vec<SolvedCell> = Vec::new();
-        for row in 0..9 {
-            for col in 0..9 {
-                if array[row][col] != 0 {
-                    solved.push(SolvedCell {
-                        cell: Cell {
-                            row: row as u8,
-                            col: col as u8,
-                        },
-                        val: array[row][col],
-                    });
-                }
-            }
-        }
-        print(&solved);
+        let solved = to_row_col()
+            .filter(|&(row, col)| array[row as usize][col as usize] != 0)
+            .map(|(row, col)| SolvedCell {
+                cell: Cell { row, col },
+                val: array[row as usize][col as usize],
+            })
+            .collect();
+
+        println!("{}", to_string(&solved));
         solve(&solved)
     }
 
     fn solve(solved: &Vec<SolvedCell>) -> Option<Vec<SolvedCell>> {
         //  未入力のセルを抽出
-        let mut unsolved: Vec<Cell> = Vec::new();
-        for row in 0..9 {
-            for col in 0..9 {
-                if !solved
+        let unsolved = to_row_col()
+            .filter(|&(row, col)| {
+                !solved
                     .iter()
                     .any(|&solved_cell| solved_cell.cell.row == row && solved_cell.cell.col == col)
-                {
-                    unsolved.push(Cell { row, col });
-                }
-            }
-        }
+            })
+            .map(|(row, col)| Cell { row, col })
+            .collect::<Vec<Cell>>();
 
         if unsolved.is_empty() {
             //  未入力のセルがない？
@@ -97,8 +98,7 @@ pub mod sudoku {
                 let answers: Vec<Vec<SolvedCell>> = unused
                     .iter()
                     .filter_map(|&val| {
-                        let new_solved =
-                            [solved.clone(), vec![SolvedCell { cell, val }]].concat();
+                        let new_solved = [solved.clone(), vec![SolvedCell { cell, val }]].concat();
                         solve(&new_solved)
                     })
                     .collect::<Vec<Vec<SolvedCell>>>();
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 // src/main.rs
 fn main() {
     use number_place::sudoku::solve_from_array;
-    use number_place::sudoku::print;
+    use number_place::sudoku::to_string;
     // 初期状態の盤面
     let array = [
-            [0,0,0,0,0,0,0,0,0],
-            [0,1,6,0,0,0,5,9,0],
-            [4,0,0,3,0,7,0,0,2],
-            [7,0,0,0,5,0,0,0,1],
-            [8,0,0,0,0,0,0,0,9],
-            [0,6,0,0,0,0,0,7,0],
-            [0,0,5,0,0,0,6,0,0],
-            [0,0,0,2,0,3,0,0,0],
-            [0,0,0,0,7,0,0,0,0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 6, 0, 0, 0, 5, 9, 0],
+        [4, 0, 0, 3, 0, 7, 0, 0, 2],
+        [7, 0, 0, 0, 5, 0, 0, 0, 1],
+        [8, 0, 0, 0, 0, 0, 0, 0, 9],
+        [0, 6, 0, 0, 0, 0, 0, 7, 0],
+        [0, 0, 5, 0, 0, 0, 6, 0, 0],
+        [0, 0, 0, 2, 0, 3, 0, 0, 0],
+        [0, 0, 0, 0, 7, 0, 0, 0, 0],
     ];
 
     if let Some(answers) = solve_from_array(&array) {
         println!("Solved!.");
-        print(&answers)
-    }
-    else {
+        println!("{}", to_string(&answers));
+    } else {
         println!("No solution found.");
     }
 }
